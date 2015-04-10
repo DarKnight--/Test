1b715ae60b043dc0a20e90efddd2d6e737bafdee
DarKnight <amit.gupta.ece13@iitbhu.ac.in>
2015-04-10 11:37:59 +0530

    completed till chapter 6. removed all errors

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -65,5 +65,5 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotEqual(francis_list_url, edith_list_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assettNotIn('Buy milk', page_text)
+		self.assertIn('Buy milk', page_text)
 		self.fail('finish the test!')
